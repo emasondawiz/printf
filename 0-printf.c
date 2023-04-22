d1c859b3b3b7f8bed76b2cc7786220ba74a4316d
emasondawiz <emaloson4vgc@gmail.com>
2023-04-22 19:42:02 +0100

    version 4 of 0-printf.c project

--- 0-printf.c
+++ 0-printf.c
@@ -10,38 +10,43 @@
 int _printf(const char *format, ...)
 {
 	int i, j, length, lenstr;
+
 	va_list myaps;
+
 	char *mystr = "";
 
 	length = strlen(format);
 
 	va_start(myaps, format);
-
 	for (i = 0; i < length; i++)
 	{
 		if (format[i] != '%')
 		{
 			_putchar(format[i]);
 		}
-		else if(format[i] == '%')
+		else if (format[i] == '%')
 		{
 			if (format[i + 1] == 'c')
 			{
 				_putchar(va_arg(myaps, int));
 				i++;
 			}
-			else if(format[i + 1] == 's')
+			else if (format[i + 1] == 's')
 			{
 				mystr = va_arg(myaps, char *);
-
-				lenstr =strlen(mystr);
-                for (j = 0; j < lenstr; j++){
-                    _putchar(mystr[j]);
-                }
-                i++;
-
-            }else{
-                _putchar(format[i]);
-            }
+				lenstr = strlen(mystr);
+				for (j = 0; j < lenstr; j++)
+				{
+					_putchar(mystr[j]);
+				}
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+			}
+		}
+	}
+	va_end(myaps);
+	return (0);
 }
-
