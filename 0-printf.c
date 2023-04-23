ab1fec881a791c54570a53b57acee0bc7b9ccfb1
0ctoberZA <tsakanilindsay@gmail.com>
2023-04-23 00:56:56 -0700

    testing

--- 0-printf.c
+++ 0-printf.c
@@ -9,44 +9,17 @@
 
 int _printf(const char *format, ...)
 {
-	int i, length;
-	va_list myaps;
 
-	char *mystr = "";
+}
 
-	length = strlen(format);
-
-	va_start(myaps, format);
-	for (i = 0; i < length; i++)
-	{
-		if (format[i] != '%')
-			putchar(format[i]);
-		else
-		{
-			if (format[i + 1] == 'c')
-			{
-				i++;
-				putchar(va_arg(myaps, int));
-			}
-			else if (format[i + 1] == 's')
-			{
-				i++;
-				mystr = va_arg(myaps, char *);
-				printf("%s", mystr);
-			}
-			else if (format[i + 1] == '%')
-			{
-				putchar(format[i]);
-				i++;
-			}
-			else
-			{
-				putchar(format[i]);
-				i++;
-				putchar('%');
-			}
-		}
-	}
-	va_end(myaps);
-	return (i);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
