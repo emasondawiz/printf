ec62962ac8b698c613b780165126262482b8ad72
0ctoberZA <tsakanilindsay@gmail.com>
2023-04-23 01:13:05 -0700

    testing

--- 0-printf.c
+++ 0-printf.c
@@ -9,7 +9,48 @@
 
 int _printf(const char *format, ...)
 {
+	int i, j, length, lenstr, counter = 0;
+	va_list myaps;
+	char *mystr = "";
 
+	length = strlen(format);
+
+	va_start(myaps, format);
+	for (i = 0; i < length; i++)
+	{
+		if (format[i] != '%')
+		{
+			counter = counter + 1;
+			_putchar(format[i]);
+		}
+		else if (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
+			{
+				counter = counter + 1;
+				_putchar(va_arg(myaps, int));
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				mystr = va_arg(myaps, char *);
+				lenstr = strlen(mystr);
+				for (j = 0; j < lenstr; j++)
+				{
+					_putchar(mystr[j]);
+					counter = counter + 1;
+				}
+				i++;
+			}
+			else
+			{
+				counter = counter + 1;
+				_putchar(format[i]);
+			}
+		}
+	}
+	va_end(myaps);
+	return (counter - 1);
 }
 
 /**
